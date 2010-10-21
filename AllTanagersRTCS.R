d4f0c26b57d42cd91c30b4ccd009eadf1330e795
allisonjshultz <allisonjshultz@bdf19f32-9af4-dfe9-beb2-e6043f54ef55>
2010-10-21 23:01:50 +0000

    The first verison of rtetracolorspace

---
+++ AllTanagersRTCS.R
@@ -0,0 +1,22 @@
+source("/Users/allisonshultz/Desktop/Rtetracolorspace/RtcsFunctions.R")
+load("bluetitss.dat")
+
+files <- list.files("/Users/allisonshultz/Desktop/RefDataPythonResults/RefDataSpeciesSummary")
+
+spesplit <- strsplit(files,split="_")
+spe <- vector()
+for (i in 1:length(spesplit)){
+	spe[i]<-spesplit[[i]][1]
+	}
+
+patchsumm <- matrix(nrow=length(files),ncol=10)
+for (i in 1:length(files)){
+	refs <- read.csv(paste("/Users/allisonshultz/Desktop/RefDataPythonResults/RefDataSpeciesSummary/",files[i],sep=""))
+	summ <- summaryOfpatches(refs,ss)
+	patchsumm[i,]<-summ
+	print(files[i])
+	}
+rownames(patchsumm)<-spe
+colnames(patchsumm)<-c("AvgSpan","VarSpan","MaxSpan","Volume","AvgHueDisp","VarHueDisp","MaxHueDisp","AvgBrill","AvgChroma","AvgAchChroma")
+
+write.csv(patchsumm, file="RTCS_PatchSum_take2.csv")
